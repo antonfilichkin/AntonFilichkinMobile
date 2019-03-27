ffcb889d10152bb1c91c641921c4fb916a513c88
afilichkin <a.filichkin@mail.ru>
2019-03-27 12:59:07 +0300

    Temp commit

---
+++ src/main/java/beans/ADC.java
@@ -0,0 +1,12 @@
+
+package beans;
+
+import lombok.Data;
+
+@Data
+@SuppressWarnings("unused")
+public class ADC {
+
+    private DesiredCapabilities desiredCapabilities;
+
+}
---
+++ src/main/java/beans/DesiredCapabilities.java
@@ -0,0 +1,16 @@
+
+package beans;
+
+import lombok.Data;
+
+@Data
+@SuppressWarnings("unused")
+public class DesiredCapabilities {
+
+    private String browserName;
+    private String deviceName;
+    private String platformName;
+    private String platformVersion;
+    private String udid;
+
+}
